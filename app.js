0617145bf85e7fe59e7fc3203ec31a3e644eeff4
lorraynerezende <lorraynerezende27@gmail.com>
2025-03-17 20:36:41 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    let input = document.getElementById("amigo");
+    let nome = input.value.trim();
+    
+    if (nome === "") {
+        alert("Por favor, insira um nome.");
+        return;
+    }
+    
+    amigos.push(nome);
+    //input.value = "";
+    atualizarLista();
+}
+
+function atualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    
+    amigos.forEach(amigo => {
+        let item = document.createElement("li");
+        item.textContent = amigo;
+        lista.appendChild(item);
+    });
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("A lista de amigos está vazia. Adicione nomes antes de sortear.");
+        return;
+    }
+    
+    let indiceSorteado = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indiceSorteado];
+    
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<li>Amigo sorteado: <strong>${amigoSorteado}</strong></li>`;
+}
